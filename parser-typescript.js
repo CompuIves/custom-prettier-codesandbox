6195d55bc82aa965d23587cb577b9beacd3dec90
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-09-01 16:43:50 +0200

    Bump version

--- parser-typescript.js
+++ parser-typescript.js
@@ -232,9 +232,9 @@ var process = {
   uptime: uptime
 };
 
-var __dirname = '/Users/i_van_hoorne/Documents/dev/custom-prettier-codesandbox/dist';
+var __dirname = '/Users/vjeux/random/prettier/dist';
 
-var __filename = '/Users/i_van_hoorne/Documents/dev/custom-prettier-codesandbox/dist/parser-typescript.js';
+var __filename = '/Users/vjeux/random/prettier/dist/parser-typescript.js';
 
 var browser$1 = true;
 
